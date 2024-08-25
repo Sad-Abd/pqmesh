01a0ab940e8b5c6ffa3b955d8d06abf2815e06c3
Sad-Abd <abedisadjad@gmail.com>
2024-08-25 21:49:27 +0330

    Revised _wildcard_ import of the geometry module

--- src/quadtree.py
+++ src/quadtree.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-from geometry import *
-
+import numpy as np
+import geometry as gm
 
 class Cell:
     def __init__(self, x, y, width, height, point=None):
@@ -15,31 +15,30 @@ class Cell:
 
 class Quadtree:
     def __init__(self, boundary, capacity=1):
-        self.root = (Cell
-                     (boundary[0], boundary[1], boundary[2], boundary[3], capacity))
+        self.root = Cell(boundary[0], boundary[1], boundary[2], boundary[3], capacity)
 
 
-def plot_shape(shape, num_points, marker='o'):
+def plot_shape(shape, num_points, marker="o"):
     fig, ax = plt.subplots()
     points = shape.to_points(num_points)
     points_array = np.array([(point.x, point.y) for point in points])
-    ax.plot(points_array[:, 0], points_array[:, 1], marker, color='k')
-    plt.axis('equal')
+    ax.plot(points_array[:, 0], points_array[:, 1], marker, color="k")
+    plt.axis("equal")
     plt.show()
 
 
 num_points = 40
 
-circle = Circle(50, 50, 40, material=1)
+circle = gm.Circle(50, 50, 40, material=1)
 plot_shape(circle, num_points)
 
-square = Square(20, 20, 30, material=2)
+square = gm.Square(20, 20, 30, material=2)
 plot_shape(square, num_points)
 
-rectangle = Rectangle(20, 20, 30, 10, material=2)
+rectangle = gm.Rectangle(20, 20, 30, 10, material=2)
 plot_shape(rectangle, num_points)
 
-multi_shape = MultiPartShape(material=1)
-multi_shape.add_part(Rectangle(10, 10, 20, 10, material=1))
-multi_shape.add_part(Circle(10, 10, 2, material=2), is_hole=True)
+multi_shape = gm.MultiPartShape(material=1)
+multi_shape.add_part(gm.Rectangle(10, 10, 20, 10, material=1))
+multi_shape.add_part(gm.Circle(10, 10, 2, material=2), is_hole=True)
 plot_shape(multi_shape, num_points)
